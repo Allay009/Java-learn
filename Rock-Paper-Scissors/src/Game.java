a1156f5889130a76652dcca60d811324ae545a8e
Allay009 <Allaycookie0721@outlook.com>
2025-11-04 14:09:59 -0500

    update

--- Rock-Paper-Scissors/src/Game.java
+++ Rock-Paper-Scissors/src/Game.java
@@ -1,16 +1,25 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.Action;
+
 public class Game {
-    
+    public boolean running = true;
+
     public int Action_self;
     public int Action_bot;
 
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
     public static void main(String[] args) {
-        while (running){
-            //int Action_bot ==
+        Game game = new Game();
+
+        while (game.running){
+            game.Action_bot = game.random.nextInt(3);
+            System.out.println("let's play the game\npress 0 is rock, press 1 is paper, press 2 is the scissors");
+
+            game.running = false;
       }
     }
-
-   
 }
